0649d7c78a5771b3ab53ffad2e7c00f29c04e91f
Nicole Riel <nikki@riel.deals>
2025-04-01 17:13:22 -0500

    fixed typo in run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ PROGRAM="$HOME/.package-log/PackageLogGui.pyw"
 STARTIN="$HOME/.package-log"
 
 cd "$STARTIN"
-source "$VENV_DIR/bin/activate" && python $"PROGRAM"
+source "$VENV_DIR/bin/activate" && python "$PROGRAM"
